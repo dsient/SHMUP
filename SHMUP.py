35ba73984c27a88762238628b88e946b79a46cc1
base6man <keatonmitchell000@hotmail.com>
2025-01-24 20:51:08 -0600

    Update SHMUP.py

--- SHMUP.py
+++ SHMUP.py
@@ -15,6 +15,7 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
+DARK_GRAY = (100, 100, 100)
 
 # Initialize pygame and create the window
 pygame.init()
@@ -42,26 +43,54 @@ class Player(pygame.sprite.Sprite):
         self.image = player_img
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
-        self.rect.bottom = HEIGHT - 10
+        self.rect.bottom = HEIGHT * 3/4
         self.speedx = 0
+<<<<<<< Updated upstream
         self.acceleration = 0.2
         self.health = 1 
 
+=======
+        self.speedy = 0
+
+        self.run_speed = 0
+        self.bubble_speed = -3
+        self.acceleration = 0.1
+        self.up_acceleration = 0.1
+        self.gravity = 0.4
+
+        self.health = 1
+>>>>>>> Stashed changes
 
     def update(self):
         keystate = pygame.key.get_pressed()
         if keystate[pygame.K_LEFT]:
-            self.speedx += self.acceleration * (-8 - self.speedx)
-        elif keystate[pygame.K_RIGHT]:
-            self.speedx += self.acceleration * ( 8 - self.speedx)
-        else:
-            self.speedx += self.acceleration * ( 0 - self.speedx)
+            self.speedx += self.acceleration * (-self.run_speed - self.speedx)
+        if keystate[pygame.K_RIGHT]:
+            self.speedx += self.acceleration * ( self.run_speed - self.speedx)
+        if not keystate[pygame.K_LEFT] and not keystate[pygame.K_RIGHT]:
+            self.speedx += self.acceleration * (                - self.speedx)
+
+        
+        if keystate[pygame.K_UP]:
+            self.speedy += self.up_acceleration * (self.bubble_speed - self.speedy)
+            global is_started
+            is_started = True
+        elif is_started: self.speedy += self.gravity
         
         self.rect.x += self.speedx
+        self.rect.y += self.speedy
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
+
+        is_hit = pygame.sprite.spritecollide(self, mobs, False)
+        if is_hit: self.health -= 1
+        if self.health == 0: pygame.quit()
+
+        for i in pygame.sprite.spritecollide(self, winds, False):
+            if not i.push_speedx == 0: self.speedx += 0.2 * (i.push_speedx - self.speedx)
+            if not i.push_speedy == 0: self.speedy += 0.2 * (i.push_speedy - self.speedy)
 
     def shoot(self):
         bullet = Bullet(self.rect.centerx, self.rect.top)
@@ -75,9 +104,15 @@ class Mob(pygame.sprite.Sprite):
         self.image.fill(RED)
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(WIDTH - self.rect.width)
+<<<<<<< Updated upstream
         self.rect.y = random.randrange(-100, -40)
         self.speedy = random.randrange(1, 8)
         self.speedx = random.randrange(-3, 3)
+=======
+        self.rect.y = -10
+        self.speedy = 4
+        self.speedx = 0
+>>>>>>> Stashed changes
 
     def update(self):
         self.rect.x += self.speedx
@@ -97,26 +132,71 @@ class Bullet(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.bottom = y
         self.rect.centerx = x
+
+        self.speedx = random.randrange(-3, 3)
         self.speedy = -10
 
     def update(self):
+        self.rect.x += self.speedx
         self.rect.y += self.speedy
         # Kill the bullet if it moves off the top of the screen
         if self.rect.bottom < 0:
             self.kill()
 
+<<<<<<< Updated upstream
 # Sprite groups
+=======
+        self.speedx *= 0.98
+        self.speedy *= 0.98
+        
+        for i in pygame.sprite.spritecollide(self, winds, False):
+            if not i.push_speedx == 0: self.speedx += 0.2 * (i.push_speedx - self.speedx)
+            if not i.push_speedy == 0: self.speedy += 0.2 * (i.push_speedy - self.speedy)
+
+class Wind(pygame.sprite.Sprite):
+    def __init__(self, push_speedx = 5):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((WIDTH, 60))
+        self.image.fill(DARK_GRAY)
+
+        self.rect = self.image.get_rect()
+        self.x = WIDTH/2
+        self.y = -30
+        
+        self.speedy = 4
+        self.push_speedx = push_speedx
+        self.push_speedy = 0
+
+    def update(self):
+        self.rect.y += self.speedy
+        
+
+is_started = False
+
+>>>>>>> Stashed changes
 all_sprites = pygame.sprite.Group()
+back_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
+winds = pygame.sprite.Group()
 bullets = pygame.sprite.Group()
 
 # Create player and mobs
 player = Player()
 all_sprites.add(player)
+<<<<<<< Updated upstream
 for i in range(8):
     m = Mob()
     all_sprites.add(m)
     mobs.add(m)
+=======
+
+mob_spawn_index = 0
+mob_spawns = [0.5, 2, 2, 2.9, 3.1, 3.3, 3.5, 5.6, 5.7, 5.8, 5.9, 6, 6.1, 6.2]
+
+wind_spawn_index = 0
+
+runtime = 0
+>>>>>>> Stashed changes
 
 # Game loop
 running = True
@@ -124,6 +204,14 @@ while running:
     # Keep loop running at the right speed
     clock.tick(FPS)
 
+<<<<<<< Updated upstream
+=======
+    if is_started:
+        runtime += clock.get_time() / 1000
+    else:
+        decoy = 0
+    
+>>>>>>> Stashed changes
     # Process input (events)
     for event in pygame.event.get():
         # Check for closing window
@@ -134,6 +222,7 @@ while running:
                 player.shoot()
 
     # Update
+    back_sprites.update()
     all_sprites.update()
 
     # Check for collisions
@@ -142,7 +231,10 @@ while running:
         m = Mob()
         all_sprites.add(m)
         mobs.add(m)
+    while runtime > 1.2 * wind_spawn_index:
+        wind_spawn_index += 1
 
+<<<<<<< Updated upstream
     # Check if a mob hits the player
     hits = pygame.sprite.spritecollide(player, mobs, False)
     if hits:
@@ -157,6 +249,17 @@ while running:
     # Draw / render
     screen.fill(BLACK)
     screen.blit(background, background_rect)
+=======
+        push_speedx = 5
+        if wind_spawn_index % 2 == 0: push_speedx = -5
+        w = Wind(push_speedx)
+        back_sprites.add(w)
+        winds.add(w)
+
+    # Draw / render
+    screen.fill(BLACK)
+    back_sprites.draw(screen)
+>>>>>>> Stashed changes
     all_sprites.draw(screen)
 
     # Flip the display
