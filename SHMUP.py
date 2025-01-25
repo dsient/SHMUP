b9a027198d41099e9118cdcdefbe9bed2604dcf4
DSiENT <54922898+dsient@users.noreply.github.com>
2025-01-24 19:10:01 -0600

    v1

--- SHMUP.py
+++ SHMUP.py
@@ -16,6 +16,7 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
+
 # initialize pygame and create window
 pygame.init()
 pygame.mixer.init()
@@ -23,11 +24,28 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Shmup!")
 clock = pygame.time.Clock()
 
+#Load BG
+background = pygame.image.load(path.join(img_dir, 'space.jpg')).convert()
+background_rect = background.get_rect
+
+#Draw BG
+screen.fill(BLACK)
+screen.blit(background, background_rect)
+all_sprites.draw(screen)
+
+# Load all game graphics
+player_img = pygame.image.load(path.join(img_dir, "ship1.png")).convert()
+bullet_img = pygame.image.load(path.join(img_dir, "bubble.png")).convert()
+
+
+
+
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 40))
-        self.image.fill(GREEN)
+        self.image = player_img
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
